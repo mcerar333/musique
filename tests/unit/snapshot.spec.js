e88707afa6021c8bf3189213aef05e0b8f101bf4
Miro <mcerar333@gmail.com>
2021-07-07 19:45:35 +0200

    added snapshot tests for BaseLottie, NotFound & About components

--- tests/unit/snapshot.spec.js
+++ tests/unit/snapshot.spec.js
@@ -3,6 +3,12 @@ import { shallowMount, RouterLinkStub } from '@vue/test-utils';
 import SongItem from '@/components/SongItem.vue';
 import Svg from '@/directives/svg';
 
+import NotFound from '@/views/NotFound.vue';
+import BaseLottie from '@/components/BaseLottie.vue';
+import * as animationData from '@/assets/lottie/error.json';
+
+import About from '@/views/About.vue';
+
 describe('Snapshots SongItem.vue', () => {
   test('component renders correctly', () => {
     const song = {
@@ -22,6 +28,52 @@ describe('Snapshots SongItem.vue', () => {
         },
         directives: {
           Svg,
+        },
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
+
+jest.mock('lottie-web', () => ({
+  loadAnimation: () => true,
+}));
+
+describe('Snapshots BaseLottie.vue', () => {
+  test('component renders correctly', () => {
+    const wrapper = shallowMount(BaseLottie, {
+      props: {
+        options: { animationData },
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
+
+describe('Snapshots NotFound.vue', () => {
+  test('component renders correctly', () => {
+    const wrapper = shallowMount(NotFound, {
+      components: { BaseLottie },
+
+      data() {
+        return {
+          lottieOptions: { animationData },
+        };
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
+
+describe('Snapshots About.vue', () => {
+  test('component renders correctly', () => {
+    const wrapper = shallowMount(About, {
+      global: {
+        mocks: {
+          $t: msg => msg,
         },
       },
     });
